0a90cf965253c2baa702af4c9d22af090947c202
Alejandro Ortega <43015395+AlejoOrtega@users.noreply.github.com>
2022-06-10 11:04:42 -0400

    target link fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
             <h2>Full-Stack Developer</h2>
             <i> Ruby | Rails | React | JavaScript | CSS | HTML</i>
             <p>Strong experience designing, implementing, debugging and testing code.</p>
-            <p>I’m also exploring other fields like the stock market, investing and developing new features for <a href="https://www.investcipher.com/" target="_blank">Up Top Trading Community</a>, an online group that Im a co-creator.</p>
+            <p>I’m also exploring other fields like the stock market, investing and developing new features for <a href="https://www.investcipher.com/">Up Top Trading Community</a>, an online group that Im a co-creator.</p>
             </div>
         </header>
     );
